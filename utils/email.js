eb7fc26f4daae3da33847f6977bb05c90e53faff
Homesh <rameshnew586586@gmail.com>
2026-07-19 00:45:05 +0530

    fix port env

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,17 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+  tls: {
+    rejectUnauthorized: false
+  }
+})
 
 // Test the connection
 transporter.verify((error, success) => {
